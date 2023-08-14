549e35ca6b9da46c9626d7666aadf28456b02d20
Nahue <nahue.santini@gmail.com>
2023-08-14 15:18:43 -0300

    deploy

--- rick_and_morty/server/src/DB_connection.js
+++ rick_and_morty/server/src/DB_connection.js
@@ -1,23 +1,25 @@
-require("dotenv").config()
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { USER, PASSWORD, HOST, PORT, BDD } = process.env;
+const { USER, PASSWORD, HOST, PORT, BDD, DB_DEPLOY } = process.env;
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
-const sequelize = new Sequelize(
-  `postgres://${USER}:${PASSWORD}@${HOST}/rickandmorty`,
+/* const sequelize = new Sequelize(
+  `postgres://${USER}:${PASSWORD}@${HOST}/${BDD}`,
   { logging: false, native: false }
-);
+); */
+
+const sequelize = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 FavoriteModel(sequelize);
 UserModel(sequelize);
 
-const {Favorite, User} = sequelize.models
+const { Favorite, User } = sequelize.models;
 
-Favorite.belongsToMany(User, {through: 'User_Favorite'})
-User.belongsToMany(Favorite, {through: 'User_Favorite'})
+Favorite.belongsToMany(User, { through: "User_Favorite" });
+User.belongsToMany(Favorite, { through: "User_Favorite" });
 
 module.exports = {
-    ...sequelize.models,
-    conn: sequelize,
- };
+  ...sequelize.models,
+  conn: sequelize,
+};
--- rick_and_morty/server/src/index.js
+++ rick_and_morty/server/src/index.js
@@ -1,7 +1,7 @@
 const server = require("./app");
 const { conn } = require("./DB_connection");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 conn
 .sync({ alter : true }) // force : true
